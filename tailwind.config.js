f76b087793c0ffe4c5d66cff2e0d452b50458bd9
chaosdynamix <chaosdynamix@posteo.net>
2022-02-26 00:13:29 +0100

    feat(vite): create vite project with vanilla-ts template

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      '2xl': '1400px',
+    },
+    colors: {
+      'light-cyan': 'hsl(193, 38%, 86%)',
+      'neon-green': 'hsl(150, 100%, 66%)',
+      'grayish-blue': 'hsl(217, 19%, 38%)',
+      'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+    },
+    extend: {
+      fontFamily: {
+        sans: [
+          'Manrope',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
+    },
+  },
+  plugins: [],
+}
